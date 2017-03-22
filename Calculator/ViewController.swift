61a47190366e062e983f530bdf7bff1effc0c05e
Tony Caio <tonycaio@tonys-macbook-pro.local>
2017-03-22 15:23:33 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tony Caio on 3/22/17.
+//  Copyright © 2017 Sparx Worx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
